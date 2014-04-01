44a60bd32c45fcc2059da65b64f903434e2d703c
rampis <jeffking@MacBookPro-00254BD74BD2-5.local>
2014-03-31 21:00:13 -0400

    AppResearch
    git init

---
+++ W1D1/W1D1/ViewController.h
@@ -0,0 +1,36 @@
+//
+//  ViewController.h
+//  W1D1
+//
+//  Created by Jeff King on 3/31/14.
+//  Copyright (c) 2014 Media Mongoose. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+
+- (IBAction)Button1:(id)sender;
+
+@property (weak, nonatomic) IBOutlet UILabel *Label;
+
+
+
+- (IBAction)Button2:(id)sender;
+
+@property (weak, nonatomic) IBOutlet UILabel *Label2;
+
+@property (weak, nonatomic) IBOutlet UITextField *field;
+
+
+
+- (IBAction)Button3:(id)sender;
+
+@property (weak, nonatomic) IBOutlet UILabel *Label3;
+
+
+
+@end
+
