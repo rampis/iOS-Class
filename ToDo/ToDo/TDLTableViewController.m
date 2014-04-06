1cbb13fa380bfa20a477342c56ae1dc4d423d6dd
rampis <jeffking@MacBookPro-00254BD74BD2-5.local>
2014-04-06 11:06:35 -0400

    SunUpdate

--- ToDo/ToDo/TDLTableViewController.m
+++ ToDo/ToDo/TDLTableViewController.m
@@ -21,7 +21,6 @@
 }
 //id is a wild card allows to pass any object
 
-
 //- (id)initWithCoder:(NSCoder *)aDecoder
 
 - (id)initWithStyle:(UITableViewStyle)style
@@ -30,115 +29,59 @@
 self = [super initWithStyle:style];
     if (self)
         
-    
-    //self = [super initWithStyle:style];
-    //if (self)
 {
         
         NSArray * array=@[
 
-        @{@"name" : @"Jisha Obukwelu",
-        @"image" : [UIImage imageNamed:@"Jisha"],
-        @"github" : @"https://github.com/Jiobu "
+        @{@"name" : @"Jisha Obukwelu",@"image" : [UIImage imageNamed:@"Jisha"],@"github" : @"https://github.com/Jiobu "
           },
-        @{@"name" : @"Jon Fox",
-        @"image" : [UIImage imageNamed:@"Jon"],
-        @"github" : @"https://github.com/FoxJon "
+        @{@"name" : @"Jon Fox",@"image" : [UIImage imageNamed:@"Jon"],@"github" : @"https://github.com/FoxJon "
           },
-        @{@"name" : @"Austen Nolan",
-        @"image" : [UIImage imageNamed:@"Austin"],
-        @"github" : @"https://github.com/adnolan99 "
+        @{@"name" : @"Austen Nolan",@"image" : [UIImage imageNamed:@"Austin"],@"github" : @"https://github.com/adnolan99 "
           },
-        @{@"name" : @"Ali Houshmand",
-        @"image" : [UIImage imageNamed:@"Ali"],
-        @"github" : @"https://github.com/HoushmandA06 "
+        @{@"name" : @"Ali Houshmand",@"image" : [UIImage imageNamed:@"Ali"],@"github" : @"https://github.com/HoushmandA06 "
           },
-        @{@"name" : @"Ed",
-        @"image" : [UIImage imageNamed:@"Ed"],
-        @"github" : @"https://github.com/MadArkitekt "
+        @{@"name" : @"Ed Salter",@"image" : [UIImage imageNamed:@"Ed"],@"github" : @"https://github.com/MadArkitekt "
           },
-        @{@"name" : @"Jeffrey Moulds",
-        @"image" : [UIImage imageNamed:@"Jeffrey"],
-        @"github" :@"https://github.com/jdmgithub "
+        @{@"name" : @"Jeffrey Moulds",@"image" : [UIImage imageNamed:@"Jeffrey"],@"github" :@"https://github.com/jdmgithub "
           },
-        @{@"name" : @"Savitha Reddy",
-        @"image" : [UIImage imageNamed:@"Savitha"],
-        @"github"   :@"https://github.com/savithareddy "
+        @{@"name" : @"Savitha Reddy",@"image" : [UIImage imageNamed:@"Savitha"],@"github"   :@"https://github.com/savithareddy "
           },
-        @{@"name" : @"Derek Weber",
-        @"image" : [UIImage imageNamed:@"Derek"],
-        @"github" : @"https://github.com/dweber03 "
+        @{@"name" : @"Derek Weber",@"image" : [UIImage imageNamed:@"Derek"],@"github" : @"https://github.com/dweber03 "
           },
-        @{@"name" : @"Ashby",
-        @"image" : [UIImage imageNamed:@"Ashby"],
-        @"github" : @"https://github.com/athornwell "
+        @{@"name" : @"Ashby Ahornwell",@"image" : [UIImage imageNamed:@"Ashby"],@"github" : @"https://github.com/athornwell "
           },
-          
-        @{@"name" : @"Austen Johnson",
-        @"image" : [UIImage imageNamed:@"Austen"],
-        @"github" : @"https://github.com/ajohnson21 "
+        @{@"name" : @"Austen Johnson",@"image" : [UIImage imageNamed:@"Austen"],@"github" : @"https://github.com/ajohnson21 "
           },
-
-        @{@"name" : @"Jeff King",
-        @"image" : [UIImage imageNamed:@"Jeff"],
-        @"github" : @"https://github.com/rampis"
+        @{@"name" : @"Jeff King",@"image" : [UIImage imageNamed:@"Jeff"],@"github" : @"https://github.com/rampis"
           },
-
-        @{@"name" : @"Heidi",
-        @"image" : [UIImage imageNamed:@"Heidi"],
-        @"github" : @"https://github.com/justagirlcoding"
+        @{@"name" : @"Heidi",@"image" : [UIImage imageNamed:@"Heidi"],@"github" : @"https://github.com/justagirlcoding"
           },
-
-        @{@"name" : @"T.J. Mercer",
-        @"image" : [UIImage imageNamed:@"T.J."],
-        @"github" : @"https://github.com/gwanunig14"
+        @{@"name" : @"T.J. Mercer",@"image" : [UIImage imageNamed:@"T.J."],@"github" : @"https://github.com/gwanunig14"
           },
-
-        @{@"name" : @"Teddy Conyers",
-        @"image" : [UIImage imageNamed:@"Teddy"],
-        @"github" : @"https//github.com/Conyers"
+        @{@"name" : @"Teddy Conyers",@"image" : [UIImage imageNamed:@"Teddy"],@"github" : @"https//github.com/Conyers"
           },
-
-        @{@"name" : @"John Yam",
-        @"image" : [UIImage imageNamed:@"John"],
-        @"github" : @"https://github.com/yamski"
+        @{@"name" : @"John Yam",@"image" : [UIImage imageNamed:@"John"],@"github" : @"https://github.com/yamski"
           }
 
         ];
                 
-        listItems = [array mutableCopy];
-
-        
-        
-//[listItems addObject:@{@{@"name" : @"John Yam",
-                                // @"image" : [UIImage imageNamed:@"John"],
-                               // @"github" : @"https://github.com/yamski"
-//}];
-
+    listItems = [array mutableCopy];
+    
     
         self.tableView.contentInset = UIEdgeInsetsMake(50,0,0,0);
         self.tableView.rowHeight = 100;
-        
         self.tableView.separatorInset = UIEdgeInsetsMake(0, 20, 0, 20);
         
         UIView * header = [[UIView alloc] initWithFrame:CGRectMake (0,0,320,100)];
         header.backgroundColor = [UIColor blueColor];
         self.tableView.tableHeaderView = header;
         
-        
-        //UILabel  * titleHeader = [[UILabel alloc] initWithFrame:CGRectMake(15,10,320, 30)];
-        //titleHeader.text = @"Class Contacts";
-        //titleHeader .textColor = [UIColor whiteColor];
-        //[header addSubview:titleHeader];
-        
-        //UITextField * nameField = [[UITextField alloc] initWithFrame:CGRectMake(20,20,160,30)];
+    
         nameField = [[UITextField alloc] initWithFrame:CGRectMake(20,20,160,30)];
         nameField.backgroundColor = [UIColor whiteColor];
         nameField.layer.cornerRadius = 6;
-        //nameField.textColor = [UIColor colorWithRed:255/255.0 green:255/255.0f blue:250/255.0f alpha:1.0f];
-        //nameField.leftView = [[UIView alloc] initWithFrame:CGRectMake(0. 0, 10, 30)];
         nameField.leftViewMode = UITextFieldViewModeAlways;
-        
         nameField.delegate = self;
         
         [header addSubview:nameField];
@@ -149,7 +92,7 @@ self = [super initWithStyle:style];
         
         [submitButton setTitle:@"New User" forState:UIControlStateNormal];
         [submitButton addTarget:(self) action:@selector (newUser) forControlEvents:
-         UIControlEventTouchUpInside];
+        UIControlEventTouchUpInside];
         [self.view addSubview:submitButton];
         
         submitButton.titleLabel.font = [UIFont systemFontOfSize:12];
@@ -165,10 +108,10 @@ self = [super initWithStyle:style];
 
         titleHeader.textColor = [UIColor lightGrayColor];    titleHeader.font = [UIFont systemFontOfSize:26];
         [header addSubview:titleHeader];
-   
     
     }
     return self;
+
 }
 
 - (void)newUser
@@ -178,42 +121,22 @@ self = [super initWithStyle:style];
     
     nameField.text = @"";
     
-   // NSLog(@"%@",username);
-    
-    
-    
-    //NSLog(@"click");
-        
     [listItems addObject:
     @{@"name":username,
-//    @"image" : [UIImage imageNamed:@"new_user"],
     @"github" : [NSString stringWithFormat:@"https://github.com/%@",username]}
     ];
     [nameField resignFirstResponder];
     [self.tableView reloadData];
+
 }
 
 - (BOOL)textFieldShouldReturn:(UITextField *)textField
+
 {
     
     
     return YES;
 }
-
-    //-void)viewDidLoad
-//{
-   // [super viewDidLoad[]];
-    
-
-
-    
-    
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
-//}
 
 - (void)didReceiveMemoryWarning
 {
@@ -227,8 +150,7 @@ self = [super initWithStyle:style];
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
 
-    
-    // Return the number of rows in the section.
+
     return [listItems count];
     
 }
@@ -242,19 +164,8 @@ self = [super initWithStyle:style];
     if (cell == nil)
     {
         cell = [[TDLTableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"cell"];
+    
     }
-
-    
-    //int index = [indexPath row];an instance with net property of row
-    
-//    NSInteger index = indexPath.row;
-    
-    //cell.textLabel.text = listItems[index];
-    //cell.imageView.image = listImages[index];
-    
-//    NSArray * reverseArray = [[listItems reverseObjectEnumerator] allObjects];
-    
-   // NSDictionary * listItem = [self getListItem:indexPath.row];
     
     cell.profileInfo = [self getListItem:indexPath.row];
     
@@ -263,8 +174,8 @@ self = [super initWithStyle:style];
 }
 
 -(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+
 {
-    //NSArray * reverseArray =
 
     NSDictionary * listItem = [self getListItem:indexPath.row];
 
@@ -272,62 +183,12 @@ self = [super initWithStyle:style];
 }
 
 - (NSDictionary *)getListItem:(NSInteger)row
+
 {
     
     NSArray * reverseArray = [[listItems reverseObjectEnumerator] allObjects];
     return reverseArray[row];
 }
-    
-    //I am want the NSArray of listItems to be a list of everyone's names within this class.
 
-
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
-{
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
